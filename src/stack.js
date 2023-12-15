966b5d2bbea5a73b06715e20662551dca610a6ff
ru-ab <ruabdrakhmanov@gmail.com>
2023-12-15 16:14:47 +0600

    feat: implement stack methods

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	/** @type {any[]} */
+	#stack = []
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(element) {
+		this.#stack.push(element)
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		return this.#stack.pop()
+	}
+
+	peek() {
+		return this.#stack.at(-1)
+	}
 }
 
 module.exports = {
-  Stack
-};
+	Stack,
+}
